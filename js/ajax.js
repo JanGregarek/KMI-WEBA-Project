c6d734612714235bc0de91d6d28d8f38dbe73053
JanGregarek <honza.gregarek@seznam.cz>
2026-01-20 21:12:29 +0100

    sem 10

--- js/ajax.js
+++ js/ajax.js
@@ -1,6 +1,6 @@
 const el = document.getElementById("others-content");
 
-fetch("http://localhost/REST_API/users/get")
+fetch("http://localhost:3000/get")
   .then(r => r.text())
   .then(text => {
     el.textContent = text;
@@ -9,4 +9,3 @@ fetch("http://localhost/REST_API/users/get")
     el.textContent = "Error";
     console.error(err);
   });
-
